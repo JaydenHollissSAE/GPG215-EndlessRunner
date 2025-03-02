1087f3cb5891f9b2b90e3db2911e45a9cdee477e
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2025-03-02 17:57:47 +1100

    Added Cluster Movement and Spawning

--- Assets/Scripts/BlockCluster.cs
+++ Assets/Scripts/BlockCluster.cs
@@ -1,23 +1,40 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BlockCluster : MonoBehaviour
 {
     [SerializeField] private List<GameObject> blocks = new List<GameObject>();
+    [SerializeField] private GameObject reset;
+    [SerializeField] private bool generateOnStart = true;
+    private Vector2 resetNewPos;
+    private GameManager gameManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        gameManager = FindFirstObjectByType<GameManager>();
         for (int i = 0; i < transform.childCount; i++)
         {
             blocks.Add(transform.GetChild(i).gameObject);
         }
+        reset = GameObject.FindGameObjectWithTag("Reset");
+        resetNewPos = new Vector2(reset.transform.position.x, 0);
+        if (generateOnStart )
+        {
+            GenerateCluster();
+        }
+
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        transform.position = Vector2.MoveTowards(transform.position, transform.position+Vector3.left, Time.fixedDeltaTime*gameManager.speed);
+        //Debug.Log(Vector2.Distance(transform.position, reset.transform.position));
+        Vector2 newPos = new Vector2(transform.position.x, 0);
+        if (Vector2.Distance(newPos, resetNewPos) <= 5f)
         {
-            Debug.Log("ButtonPressed");
+            transform.position = new Vector2(23.26f, transform.position.y);
             GenerateCluster();
         }
     }
@@ -30,9 +47,9 @@ public class BlockCluster : MonoBehaviour
         }
 
         int toDisable = blocks.Count - 1 -(Random.Range(1, 6));
-        Debug.Log(toDisable.ToString()+" toDisable");
+        //Debug.Log(toDisable.ToString()+" toDisable");
         int disabled = 0;
-        Debug.Log(disabled.ToString()+ " disabled");
+        //Debug.Log(disabled.ToString()+ " disabled");
         while (disabled < toDisable)
         {
             GameObject item = blocks[Random.Range(0, blocks.Count)];
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,7 @@ public class GameManager : MonoBehaviour
     public string activePowerup;
     public float powerupEndTime;
     public float gameStartTime;
+    public float speed = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()

269c362689b1586d2297ee026b441034c78fb9d4
lms34 <1031566@student.sae.edu.au>
2025-03-20 09:52:49 +0800

    Added a Start Menu
    -PLAY button: Plays the game
    -OPTIONS button
          -Volume control - not setup as we do not have music yet
    -EXIT Button

---
+++ Assets/mainmenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+   
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Loading Main Game Loop");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}

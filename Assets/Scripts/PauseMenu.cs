aa738e347c3ea0bcce18b524f1bf374aa30c628d
lms34 <1031566@student.sae.edu.au>
2025-04-09 12:40:12 +0800

    UI work
    Imported a new pixel style font
    added a pause button with resume and back to home functions

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void BackToHome(int sceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+    }
+
+}
